5d2416ab7cd04f7d45ec6ea49492fbbd7f361757
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2025-05-31 12:07:07 +0700

    Initial Commit

---
+++ Naufalp6/Naufalp6App.swift
@@ -0,0 +1,17 @@
+//
+//  Naufalp6App.swift
+//  Naufalp6
+//
+//  Created by Naufal Adli on 31/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Naufalp6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
